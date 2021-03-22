6d80b22efe9d607f10dba0dda36e651ca4a5d8b9
Siva <Siva@DESKTOP-HJU38RD>
2021-03-22 14:05:08 +0530

    pushing add numbers

---
+++ SeleniumDemo1/src/AddNumDemo1.java
@@ -0,0 +1,20 @@
+
+public class AddNumDemo1 {
+	int a=10;
+	int b=20;
+	int c=a+b;
+	public static void main(String[] args) {
+		
+		AddNumDemo1 b1= new AddNumDemo1();
+		b1.funA();
+	}
+		
+
+ void funA()
+{
+		System.out.println("additional Value is:"+(a+b));
+
+	
+		System.out.println("add:"+c); 
+}
+}
